f8f2a4c206d07239ec0a191e00bb8a7c6148c8b0
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-07-05 04:06:38 +0300

    removed comment from 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,5 +3,4 @@ x = ARGV[0].scan(/((?<=from:)\w+)/).join
 y = ARGV[0].scan(/((?<=to:)\+\d+)/).join
 z = ARGV[0].scan(/(((?<=flags:)-?\d:-?\d:-?\d:-?\d:-?\d))/).join
 puts x + ',' + y + ',' + z
-#puts ARGV[0].scan(/((?<=from:)\w+)/).join + ',' + puts ARGV[0].scan(/((?<=to:)\+\d+)/).join + ',' + ARGV[0].scan(/(((?<=flags:)-?\d:-?\d:-?\d:-?\d:-?\d))/).join
 
