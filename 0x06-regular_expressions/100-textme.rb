a96b418e2bb7a98739bee6c321033d79c5c5c70f
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-07-05 04:08:39 +0300

    fixed double output 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 x = ARGV[0].scan(/((?<=from:)\w+)/).join
 y = ARGV[0].scan(/((?<=to:)\+\d+)/).join
-z = ARGV[0].scan(/(((?<=flags:)-?\d:-?\d:-?\d:-?\d:-?\d))/).join
+z = ARGV[0].scan(/((?<=flags:)-?\d:-?\d:-?\d:-?\d:-?\d)/).join
 puts x + ',' + y + ',' + z
 
