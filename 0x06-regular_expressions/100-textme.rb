7db65ca828e197c7b211378e032438995733af1e
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-07-05 04:10:18 +0300

    8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 x = ARGV[0].scan(/((?<=from:)\w+)/).join
-y = ARGV[0].scan(/((?<=to:)\+\d+)/).join
+y = ARGV[0].scan(/((?<=to:)\+?\w+)/).join
 z = ARGV[0].scan(/((?<=flags:)-?\d:-?\d:-?\d:-?\d:-?\d)/).join
 puts x + ',' + y + ',' + z
 
