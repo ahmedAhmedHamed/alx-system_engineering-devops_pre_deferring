03e2b1611743308e74f0332425788dc2475f578d
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-07-05 04:12:04 +0300

    fixed phone number on sender

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-x = ARGV[0].scan(/((?<=from:)\w+)/).join
+x = ARGV[0].scan(/((?<=from:)\+?\w+)/).join
 y = ARGV[0].scan(/((?<=to:)\+?\w+)/).join
 z = ARGV[0].scan(/((?<=flags:)-?\d:-?\d:-?\d:-?\d:-?\d)/).join
 puts x + ',' + y + ',' + z
