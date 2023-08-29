acd2704eab4f2128a5d779885aed1fd0d81d5e3e
Ahmed Hamed <ahmedahmedhamedahmed0@gmail.com>
2023-08-30 02:15:20 +0300

    7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,38 @@
+# Installing Nginx web server w Puppet
+include stdlib
+
+$required_link = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
+$full_content = "\trewrite ^/redirect_me/$ ${link} permanent;"
+
+exec { 'restart nginx':
+  command => '/usr/sbin/service nginx restart',
+  require => Package['nginx']
+}
+
+file_line { 'Set 301 redirection':
+  ensure   => 'present',
+  after    => 'server_name\ _;',
+  path     => '/etc/nginx/sites-available/default',
+  multiple => true,
+  line     => $full_content,
+  notify   => Exec['restart nginx'],
+  require  => File['/var/www/html/index.html']
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update packages']
+}
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => 'Holberton School',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root'
+}
+
+exec { 'update packages':
+  command => '/usr/bin/apt-get update'
+}
+
